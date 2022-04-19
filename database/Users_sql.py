bcba86bc38bd8868d76331bc4d73cea28343888e
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2022-04-19 20:43:02 +0530

    Create Users_sql.py

---
+++ database/Users_sql.py
@@ -0,0 +1,62 @@
+from sqlalchemy import Column, Integer, String
+from database import BASE, SESSION
+
+
+class Users(BASE):
+    __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}
+    user_id = Column(Integer, primary_key=True)
+    insta_username = Column(String)
+    insta_password = Column(String)
+
+    def __init__(self, user_id, insta_username=None, insta_password=None):
+        self.user_id = user_id
+        self.insta_username = insta_username
+        self.insta_password = insta_password
+
+
+Users.__table__.create(checkfirst=True)
+
+
+async def num_users():
+    try:
+        return SESSION.query(Users).count()
+    finally:
+        SESSION.close()
+
+
+async def set_info(user_id, username, password):
+    q = SESSION.query(Users).get(user_id)
+    if q:
+        q.insta_username = username
+        q.insta_password = password
+    else:
+        SESSION.add(Users(user_id, username, password))
+    SESSION.commit()
+
+
+async def delete_info(user_id):
+    q = SESSION.query(Users).get(user_id)
+    if q and q.insta_password:
+        q.insta_username = None
+        q.insta_password = None
+        SESSION.commit()
+        return True
+    else:
+        SESSION.close()
+        return False
+
+
+async def get_info(user_id):
+    q: Users = SESSION.query(Users).get(user_id)
+    if q:
+        if q.insta_password:
+            info = q.insta_username, q.insta_password
+            SESSION.close()
+            return info
+        else:
+            return None, None
+    else:
+        SESSION.add(Users(user_id))
+        SESSION.commit()
+        return None, None
