5f66a70ab0a3a4153c2edbe8838a334e2d6bc62f
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2022-04-19 16:34:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -57,7 +57,53 @@ async def give_filter(client,message):
 async def stickers(bot, msg):
     if msg.sticker:
         await msg.reply(f"This Sticker's ID is⚠️ `{msg.sticker.file_id}`", quote=True)
+
+@Client.on_message(filters.media & filters.private)
+async def telegraph_upload(bot, update):
     
+    if not await db.is_user_exist(update.from_user.id):
+	    await db.add_user(update.from_user.id)
+    
+    text = await update.reply_text(
+        text="<code>Downloading to My Server ...</code>",
+        disable_web_page_preview=True
+    )
+    media = await update.download()
+    
+    await text.edit_text(
+        text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>",
+        disable_web_page_preview=True
+    )
+    
+    try:
+        response = upload_file(media)
+    except Exception as error:
+        print(error)
+        await text.edit_text(
+            text=f"Error :- {error}",
+            disable_web_page_preview=True
+        )
+        return
+    
+    try:
+        os.remove(media)
+    except Exception as error:
+        print(error)
+        return
+    
+    await text.edit_text(
+        text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>Join :-</b> @FayasNoushad",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="Open Link", url=f"https://telegra.ph{response[0]}"),
+                    InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
+                ],
+                [InlineKeyboardButton(text="⚙ Join Updates Channel ⚙", url="https://telegram.me/FayasNoushad")]
+            ]
+        )
+    )    
 
 @Client.on_message(filters.command('autofilter'))
 async def fil_mod(client, message):
