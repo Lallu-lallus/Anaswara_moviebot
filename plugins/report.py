22882d192d35fcf9533bb3e16c233746d0fa580b
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-04-21 21:28:23 +0530

    Update report.py

--- plugins/report.py
+++ plugins/report.py
@@ -1,26 +1,18 @@
 import pyrogram
+import asyncio
 import os
-from pyrogram import Client, filters
-from pyrogram.types import Message, User
+from pyrogram import filters, Client as Sflix
 
-
-@Client.on_message(
-    (
-        filters.command(["report"]) |
-        filters.regex("@admins") |
-        filters.regex("@admin")
-    ) &
-    filters.group
-)
-async def report(bot, message):
+@Sflix.on_message((filters.command(["report"]) | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
+async def report_user(bot, message):
     if message.reply_to_message:
         chat_id = message.chat.id
         reporter = str(message.from_user.id)
         mention = message.from_user.mention
         admins = await bot.get_chat_members(chat_id=chat_id, filter="administrators")
-        success = False
-        report = f"Reporter : {mention} ({reporter})" + "\n"
-        report += f"Message : {message.reply_to_message.link}"
+        success = True
+        report = f"𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})" + "\n"
+        report += f"𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {message.reply_to_message.link}"
         for admin in admins:
             try:
                 reported_post = await message.reply_to_message.forward(admin.user.id)
@@ -33,4 +25,4 @@ async def report(bot, message):
             except:
                 pass
         if success:
-            await message.reply_text("**Reported to Admins!**")
+            await message.reply_text("𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝖽 𝗍𝗈 𝖠𝖽𝗆𝗂𝗇𝗌!")
