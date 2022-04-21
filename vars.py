ee775217eacb63e5468dac3ac2d2f217e9951e88
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2022-04-21 14:10:16 +0530

    Delete vars.py

--- vars.py
+++
@@ -1,7 +0,0 @@
-from os import getcwd
-
-from prettyconf import Configuration
-from prettyconf.loaders import EnvFile, Environment
-
-env_file = f"{getcwd()}/.env"
-config = Configuration(loaders=[Environment(), EnvFile(filename=env_file)])
