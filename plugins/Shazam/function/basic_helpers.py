b564d93eab558ee87342eed8e242b59b0e26913e
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-04-21 20:19:59 +0530

    Update basic_helpers.py

--- plugins/Shazam/function/basic_helpers.py
+++ plugins/Shazam/function/basic_helpers.py
@@ -1,1 +1,66 @@
+import os
+import math
+import time
+import shlex
+import asyncio
+from math import ceil
+from pyrogram import Client
+from traceback import format_exc
+from typing import Tuple, Union
+from pyrogram.errors import FloodWait, MessageNotModified
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    Message,
+)
+import mimetypes
+import functools
+import multiprocessing
+from concurrent.futures import ThreadPoolExecutor
+
+
+
+async def delete_or_pass(message):
+    """Delete Message If Its From Self Else Just Pass"""
+    AFS = await sudo_list()
+    if message.from_user.id in AFS:
+        return message
+    return await message.delete()
+
+
+def humanbytes(size):
+    """Convert Bytes To Bytes So That Human Can Read It"""
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+def run_in_exc(f):
+    @functools.wraps(f)
+    async def wrapper(*args, **kwargs):
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(exc_, lambda: f(*args, **kwargs))
+    return wrapper
+
+
+def time_formatter(milliseconds: int) -> str:
+    """Time Formatter"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + " day(s), ") if days else "")
+        + ((str(hours) + " hour(s), ") if hours else "")
+        + ((str(minutes) + " minute(s), ") if minutes else "")
+        + ((str(seconds) + " second(s), ") if seconds else "")
+        + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
+    )
+    return tmp[:-2]
 
