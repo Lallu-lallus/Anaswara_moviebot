2117b7fea73cf4634591e89ea3a46672a370ca32
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-04-24 16:19:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -46,7 +46,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker(
-            sticker="CAACAgIAAxkBAAIIE2JlKpoSbNeerLeZ3Gj3QW6Yx0mVAALTBwACRvusBP-3rWmWskNmHgQ",
+            sticker="CAACAgIAAxkBAAIIF2JlKvcVvbmEkxbta38a-4QTDSo0AAKOFQACJU3BSY8WTX7r0TbzHgQ",
            # caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup
             #parse_mode='html'
@@ -86,7 +86,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker(
-            sticker="CAACAgIAAxkBAAIIE2JlKpoSbNeerLeZ3Gj3QW6Yx0mVAALTBwACRvusBP-3rWmWskNmHgQ",
+            sticker="CAACAgIAAxkBAAIIF2JlKvcVvbmEkxbta38a-4QTDSo0AAKOFQACJU3BSY8WTX7r0TbzHgQ",
            # caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup
             #parse_mode='html'
