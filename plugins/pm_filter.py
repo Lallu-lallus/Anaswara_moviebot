927d0fb630344d5f8ad1b141243169d7b998e219
Lallu_tg <89923355+Lallu-lallus@users.noreply.github.com>
2022-05-02 09:37:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1152,7 +1152,7 @@ if SPELL_MODE:
                  InlineKeyboardButton("🔮 Reason", callback_data="reason")
                  ]]
                 )    
-                imdb=await get_poster(search)
+                    await get_poster(search)
                 if imdb and imdb.get('poster'):
                     await message.reply_photo(photo=imdb.get('poster'), caption=LuciferMoringstar.GET_MOVIE_7.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb.get('genres'), year=imdb.get('year'), rating=imdb.get('rating'), short=imdb.get('short_info'), url=imdb['url']), reply_markup=reply_markup) 
                     return
