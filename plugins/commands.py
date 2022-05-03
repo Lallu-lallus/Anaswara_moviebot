5b2f775c78d761d5fc758eea93af5ee67fd8c01b
Infamous1706 <61957669+athul1706@users.noreply.github.com>
2022-05-03 12:55:17 +0530

    copy fix

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,6 +8,7 @@ from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details
 from database.users_chats_db import db
+from database.connections_mdb import active_connection
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, LOG_CHANNEL, PICS
 from utils import get_size, is_subscribed, temp
 
